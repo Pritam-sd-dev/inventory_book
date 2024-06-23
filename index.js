900ab3d0e21e7ddef5fc895ee86ac5e0aab23512
pritam <pritam.pk36129@gmail.com>
2024-06-23 15:19:20 +0530

    add cookie parser

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { config } from 'dotenv';
-import { test } from './middlewares/test.js';
 import userRouter from './routes/user.route.js';
+import shopRouter from './routes/shop.route.js';
 import connectDB from './config/dbConnection.js';
+import cookieParser from 'cookie-parser';
 
 config();
 const PORT = process.env.PORT || 8000;
@@ -11,10 +12,11 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(test);
+app.use(cookieParser());
 
 // routes
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/shops", shopRouter);
 
 app.use('/api/v1/ping', (req, res) => {
     return res.json('server is up and running at port ' +  PORT);
