aacc5126215c8ca401b10dcdebe9bedc0f0d040e
pritam <pritam.pk36129@gmail.com>
2024-06-26 17:17:15 +0530

    fix createVariant api

--- controllers/variant.controller.js
+++ controllers/variant.controller.js
@@ -3,13 +3,17 @@ import { Variant } from "../models/models.js";
 
 export const createVariant = async (request, response) => {
     const { name } = request.body;
+    if((name) == undefined) {
+        return response
+            .status(400)
+            .json({ success: false, message: "name is not provided :(" });
+    }
     try {
         const variant = await Variant.create({name});
-        if((name) == undefined) {
-            return response
-                .status(400)
-                .json({ success: false, message: "name is not provided :(" });
-        }
+        return response.status(200).json({
+            success: true,
+            data: { variantId: variant.id },
+        });
     } catch (error) {
         return response
             .status(500)
